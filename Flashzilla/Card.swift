1c37e621b8345ac97416439959442c9de9daa1d7
Ionut Vasile <email@iovasile.dev>
2022-02-19 19:43:08 +0200

    feature: card model with example

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Ionut Vasile on 19.02.2022.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in Doctor Who?", answer: "Jodie Wittaker")
+}
